96f531ba0aaf8d60d3a8e0f572134731c32dc8d6
shivakant786 <shivakanttiwari123@gmail.com>
2024-07-11 12:29:49 +0530

    apply lazyloading and loader

--- src/router/RootRouter.jsx
+++ src/router/RootRouter.jsx
@@ -1,36 +1,47 @@
+import React, { Suspense, lazy} from "react";
 import { Routes, Route } from "react-router-dom";
 import RootLayout from "../layout/RootLayout";
-import HomePage from "../pages";
-import PagenotFound from "../pages/404";
-import Login from "../component/Login";
-import Dashboard from "../pages/Dashbord";
+const HomePage = lazy(() => import("../pages"));
+const PagenotFound = lazy(() => import("../pages/404"));
+const Login = lazy(() => import("../component/Login"));
+const Dashboard = lazy(() => import("../pages/Dashbord"));
 import ProtectedRoute from "./ProtecetdRoute";
 import PublicRoute from "./PublicRoute";
+import Loader from "../component/Loader";
 const RootRouter = () => {
+ 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<RootLayout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="*" element={<PagenotFound />} />
-        </Route>
-        <Route
-          path="/login"
-          element={
-            <PublicRoute>
-              <Login />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="loader-wr">
+            <Loader />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<RootLayout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<PagenotFound />} />
+          </Route>
+          <Route
+            path="/login"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Suspense>
     </>
   );
 };
